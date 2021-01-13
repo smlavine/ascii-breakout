03d09a0eda70dd1b2bfc6c8f702eb36719ba0092
Sebastian LaVine <mail@smlavine.com>
2021-01-12 20:52:58 -0500

    License under GPLv3.0-or-later

--- main.c
+++ main.c
@@ -1,29 +1,19 @@
-/**
+/*
  * ascii-breakout - a TUI Breakout game
- * Copyright (C) 2020 Sebastian LaVine <seblavine@outlook.com>
+ * Copyright (C) 2020-2021 Sebastian LaVine <mail@smlavine.com>
  *
- * (MIT License)
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * The software is provided "as is", without warranty of any kind, express or
- * implied, including but not limited to the warranties of merchantability,
- * fitness for a particular purpose and noninfringement. In no event shall the
- * authors or copyright holders be liable for any claim, damages or other
- * liability, whether in an action of conduct, tort or otherwise, arising from,
- * out of or in connection with the software or the use or other deailings in
- * the software.
- */
-/* Originally written for submission as the create task for APCSP 2020.
- * The up-to-date source code for this project can be found here:
- * <https://github.com/smlavine/ascii-breakout>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
  */
 
 #include <signal.h>
